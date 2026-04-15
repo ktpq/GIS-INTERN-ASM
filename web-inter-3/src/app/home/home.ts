b0fcd93cd98316bf9eb41d249362e0512c4632d6
ktpq <kartpol47@gmail.com>
2026-04-15 22:31:51 +0700

    feat:web-inter-3 setup project

---
+++ web-inter-3/src/app/home/home.ts
@@ -0,0 +1,22 @@
+import { Component, CUSTOM_ELEMENTS_SCHEMA, signal } from '@angular/core';
+import '@arcgis/map-components/components/arcgis-map'; // map component
+import { ArcgisMap } from '@arcgis/map-components/components/arcgis-map';
+
+
+@Component({
+  selector: 'app-home',
+  imports: [],
+  templateUrl: './home.html',
+  styleUrl: './home.css',
+  schemas: [CUSTOM_ELEMENTS_SCHEMA], 
+})
+export class Home {
+
+  mapComponent!: ArcgisMap;
+  arcgisViewReadyChange(event: CustomEvent) {
+    // The view is ready, add additional functionality below
+    console.log('Map is ready', event);
+    this.mapComponent = event.target as ArcgisMap;
+  }
+
+}
