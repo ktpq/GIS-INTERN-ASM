75535a89a9e53793b550a20839fdab683fef2143
ktpq <kartpol47@gmail.com>
2026-04-16 18:30:02 +0700

    feat: enhance route highlighting and selection in home component

--- web-inter-4/src/app/home/home.ts
+++ web-inter-4/src/app/home/home.ts
@@ -8,10 +8,11 @@ import * as route from "@arcgis/core/rest/route";
 import RouteParameters from "@arcgis/core/rest/support/RouteParameters.js";
 import FeatureSet from '@arcgis/core/rest/support/FeatureSet';
 import SimpleLineSymbol from '@arcgis/core/symbols/SimpleLineSymbol';
+import{ NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [NgClass],
   templateUrl: './home.html',
   styleUrl: './home.css',
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
@@ -20,6 +21,7 @@ export class Home {
   mapComponent!: ArcgisMap;
   allPoint = signal<Graphic[]>([]);
   allFeatures = signal<Graphic[]>([]);
+  isSelectedIndex = signal<number>(-1);
   arcgisViewReadyChange(event: CustomEvent) {
     // The view is ready, add additional functionality below
     console.log('Map is ready', event);
@@ -31,7 +33,46 @@ export class Home {
     })
   }
 
+  highLightRoute(index: number){
+    this.isSelectedIndex.set(index);
+    this.removeOnlyPolyline()
+    this.allFeatures().forEach((feature, i) => {
+      let lineStyle = null;
 
+      // line ที่เลือก
+      if (i === index){
+        lineStyle = new SimpleLineSymbol({
+          color: 'yellow', 
+          width: 4,
+        })
+
+        this.mapComponent.view.goTo(
+          feature.geometry!.extent!.clone().expand(1.5), 
+          { duration: 800 }
+        );
+      // line ที่ไม่ได้เลือก
+      } else {
+        lineStyle = new SimpleLineSymbol({
+          color: 'green', 
+          width: 2,
+        })
+      }
+      const lineGraphic = new Graphic({
+          geometry: feature.geometry,
+          symbol: lineStyle,
+        })
+      this.mapComponent.view.graphics.add(lineGraphic);
+    })
+  }
+
+  removeOnlyPolyline() {
+    const polylineGraphics = this.mapComponent.view.graphics.toArray().filter(graphic => {
+      return graphic.geometry?.type === "polyline"; 
+    });
+
+    this.mapComponent.view.graphics.removeMany(polylineGraphics);
+    
+  }
   findRoute(){
     const url = "https://sampleserver6.arcgisonline.com/arcgis/rest/services/NetworkAnalysis/SanDiego/NAServer/Route"
     const routeParams = new RouteParameters({
@@ -41,8 +82,9 @@ export class Home {
       returnDirections: true,
     })
     route.solve(url, routeParams).then((response) => { 
-      console.log("route data : ",response)
+      
       if (response.routeResults[0].directions){
+        console.log("route data : ",response.routeResults[0].directions.features)
         this.allFeatures.set(response.routeResults[0].directions.features);
         this.allFeatures().forEach((feature) => {
           const lineStyle = new SimpleLineSymbol({
@@ -94,6 +136,7 @@ export class Home {
   clearAllPoints(){
     this.allPoint.set([]);
     this.allFeatures.set([])
+    this.isSelectedIndex.set(-1);
     this.mapComponent.view.graphics.removeAll();
   }
 }
