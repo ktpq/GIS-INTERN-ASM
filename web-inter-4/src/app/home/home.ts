20dd2ba4ad2f99a4753ce0f3be3571aaeea54c54
ktpq <kartpol47@gmail.com>
2026-04-16 17:06:18 +0700

    feat: implement route finding and point management in home component

--- web-inter-4/src/app/home/home.ts
+++ web-inter-4/src/app/home/home.ts
@@ -1,6 +1,12 @@
-import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA, signal } from '@angular/core';
 import '@arcgis/map-components/components/arcgis-map'; // map component
 import { ArcgisMap } from '@arcgis/map-components/components/arcgis-map';
+import Graphic from '@arcgis/core/Graphic';
+import SimpleMarkerSymbol from '@arcgis/core/symbols/SimpleMarkerSymbol';
+import Point from '@arcgis/core/geometry/Point';
+import * as route from "@arcgis/core/rest/route";
+import RouteParameters from "@arcgis/core/rest/support/RouteParameters.js";
+import FeatureSet from '@arcgis/core/rest/support/FeatureSet';
 
 @Component({
   selector: 'app-home',
@@ -11,9 +17,63 @@ import { ArcgisMap } from '@arcgis/map-components/components/arcgis-map';
 })
 export class Home {
   mapComponent!: ArcgisMap;
+  allPoint = signal<Graphic[]>([]);
   arcgisViewReadyChange(event: CustomEvent) {
     // The view is ready, add additional functionality below
     console.log('Map is ready', event);
     this.mapComponent = event.target as ArcgisMap;
+
+    this.mapComponent.view.on('click', (event) => {
+      console.log('Map clicked at: ', event.mapPoint);
+      this.addPointGraphic(event.mapPoint);
+    })
+  }
+
+
+  findRoute(){
+    const url = "https://sampleserver6.arcgisonline.com/arcgis/rest/services/NetworkAnalysis/SanDiego/NAServer/Route"
+    const routeParams = new RouteParameters({
+      stops: new FeatureSet({
+        features: [...this.allPoint()]
+      }),
+      returnDirections: true,
+    })
+    route.solve(url, routeParams).then((response) => { console.log("route data : ",response) })
+  }
+  addPointGraphic(mapPoint: Point){
+    let pointStyle = null;
+      if (this.allPoint().length === 0) {
+        pointStyle = new SimpleMarkerSymbol({
+          color: 'blue',
+          size: 10,
+          outline: {
+            color: 'white',
+            width: 1,
+          },
+        });
+      } else {
+        pointStyle = new SimpleMarkerSymbol({
+          color: 'red',
+          size: 10,
+          outline: {
+            color: 'white',
+            width: 1,
+          },
+        });
+      }
+        
+      const pointGraphic = new Graphic({
+        geometry: mapPoint,
+        symbol: pointStyle,
+      })
+
+      this.mapComponent.view.graphics.add(pointGraphic);
+      this.allPoint.update((points) => [...points, pointGraphic]);
+      console.log("this is array of allPoint",this.allPoint)
+  }
+
+  clearAllPoints(){
+    this.allPoint.set([]);
+    this.mapComponent.view.graphics.removeAll();
   }
 }
