24d3af0b6ab97fa2dbbfd79c903a0b48d4eb7453
ktpq <kartpol47@gmail.com>
2026-04-17 21:57:53 +0700

    feat:finish web-inter-5

--- web-inter-5/src/app/home/home.ts
+++ web-inter-5/src/app/home/home.ts
@@ -2,6 +2,9 @@ import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 import '@arcgis/map-components/components/arcgis-map'; // map component
 import { ArcgisMap } from '@arcgis/map-components/components/arcgis-map';
+import "@arcgis/map-components/components/arcgis-layer-list";
+import TileLayer from '@arcgis/core/layers/TileLayer';
+import FeatureLayer from '@arcgis/core/layers/FeatureLayer';
 
 @Component({
   selector: 'app-home',
@@ -16,5 +19,20 @@ export class Home {
     // The view is ready, add additional functionality below
     console.log('Map is ready', event);
     this.mapComponent = event.target as ArcgisMap;
+
+    if (this.mapComponent){
+      const oceanMap = new TileLayer({
+        url: "https://services.arcgisonline.com/arcgis/rest/services/Ocean/World_Ocean_Base/MapServer"
+      })
+
+      const cencusMap = new FeatureLayer({
+        url: "https://sampleserver6.arcgisonline.com/arcgis/rest/services/Census/MapServer",
+        outFields: ["*"],
+      })
+
+      this.mapComponent.view.map.add(oceanMap);
+      this.mapComponent.view.map.add(cencusMap);
+
+    }
   }
 }
